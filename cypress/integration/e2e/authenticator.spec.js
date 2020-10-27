8d2248b48353cdb03f2683721ffc85460768a5fa
QA1980 <55928283+QA1980@users.noreply.github.com>
2020-10-27 14:38:26 -0400

    Remove comments

--- cypress/integration/e2e/authenticator.spec.js
+++ cypress/integration/e2e/authenticator.spec.js
@@ -9,7 +9,7 @@ describe("Authenticator:", function () {
   // Note that this takes into account the base url that we///////
   // already configured. So the final address will be
   // "http://localhost:3000/"///////
-  //test add pull request review 
+  
   
   beforeEach(function () {
     cy.visit("/");
